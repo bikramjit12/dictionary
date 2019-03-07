2337fc014a1375624dc9034a703e6ac38795d131
Bikramjit Singh Mall <BM042@atcltd.atc.org.nz>
2019-03-07 14:26:10 +1300

    Add project files.

---
+++ dictionary/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxWords = new System.Windows.Forms.ListBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtInputWord = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbxWords
+            // 
+            this.lbxWords.FormattingEnabled = true;
+            this.lbxWords.Location = new System.Drawing.Point(638, 54);
+            this.lbxWords.Name = "lbxWords";
+            this.lbxWords.Size = new System.Drawing.Size(120, 95);
+            this.lbxWords.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(211, 54);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load Word";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(418, 54);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click_1);
+            // 
+            // txtInputWord
+            // 
+            this.txtInputWord.Location = new System.Drawing.Point(41, 56);
+            this.txtInputWord.Name = "txtInputWord";
+            this.txtInputWord.Size = new System.Drawing.Size(100, 20);
+            this.txtInputWord.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInputWord);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.lbxWords);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxWords;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtInputWord;
+    }
+}
+
---
+++ dictionary/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dictionary
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> DictionaryWords = new Dictionary<string,
+string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            //make a counter that shows how many words are loaded (really just something to look at)
+            int count = 0;
+            //a great way of loading each line of a file into your program
+            foreach (string line in File.ReadLines(@"C:\Users\vicky\source\repos\dictionary\dictionary\bin\Debug\dict1.txt"))
+            {
+                //create the counter, show it on the top of the form
+                count++;
+                this.Text = count.ToString();
+                //if the dictionary doesn't have the word then load it. Otherwise it crashes when it finds a conflict with an existing word
+                if (DictionaryWords.ContainsKey(line) == false)
+                {
+                    //add key / value with both the same word
+                    DictionaryWords.Add(line, line);
+                }
+            } //shows when the words are loaded
+            this.Text = "Words Loaded";
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        { 
+        }
+
+        private void btnSearch_Click_1(object sender, EventArgs e)
+        {
+            //input the search word
+            string SearchWord = txtInputWord.Text;
+            //if the word is in the dictionary
+            if (DictionaryWords.ContainsKey(SearchWord))
+            {
+                this.Text = SearchWord + " is found";
+                lbxWords.Items.Add(SearchWord + " is found");
+            }
+            else
+            { //the word isn't in the dictionary
+                this.Text = SearchWord + " does not exist";
+                lbxWords.Items.Add(SearchWord + " does not exist");
+
+            }
+        }
+    }
+}
